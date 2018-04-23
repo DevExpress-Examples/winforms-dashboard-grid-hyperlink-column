649795675787b2b7b823d88f0d61875b011b0132
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_GridHyperlinkColumn/Form1.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DataAccess.Excel;
+
+namespace Dashboard_GridHyperlinkColumn
+{
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dashboardDesigner1.CreateRibbon();
+
+            Dashboard dashboard = new Dashboard();
+
+            DashboardExcelDataSource excelDataSource = new DashboardExcelDataSource("Excel Data Source");
+            excelDataSource.FileName = @"..\..\Data\GDPByCountry.xlsx";
+            excelDataSource.SourceOptions = new ExcelSourceOptions(new ExcelWorksheetSettings("Sheet1"));
+            dashboard.DataSources.Add(excelDataSource);
+
+            GridDashboardItem grid = new GridDashboardItem();
+            grid.DataSource = excelDataSource;
+
+            // Creates two hyperlink columns: the first column takes hyperlinks from the underlying data source while the second 
+            // generates links based on the specified URI pattern and data source country names.
+            GridHyperlinkColumn hyperlinkColumn1 = new GridHyperlinkColumn(new Dimension("Uri"), new Dimension("OfficialName"));
+            GridHyperlinkColumn hyperlinkColumn2 = new GridHyperlinkColumn(new Dimension("Name"), new Dimension("OfficialName"));
+            hyperlinkColumn2.UriPattern = "https://en.wikipedia.org/wiki/{0}";
+
+            GridMeasureColumn gdpColumn = new GridMeasureColumn(new Measure("GDP"));
+            grid.Columns.AddRange(hyperlinkColumn1, hyperlinkColumn2, gdpColumn);
+            dashboard.Items.Add(grid);
+            dashboardDesigner1.Dashboard = dashboard;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# WinForms Designer - How to create the Grid's hyperlink column in code
+
+
+This example shows how to create two Grid hyperlink columns in code. The first column takes hyperlinks from the underlying data source while the second one generates links based on the specified URI pattern and data source country names.
+
+<br/>
+
+
